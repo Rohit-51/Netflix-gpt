39e93b1732573041ce8d5d5a87c7c164f0c47807
Rohit <rohit@Rohits-MacBook-Air.local>
2025-11-26 07:42:15 +0530

    cra & tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-center items-center'>This is just the start!</div>
+    </>
+  )
+}
+
+export default App
